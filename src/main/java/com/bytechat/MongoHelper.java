9a05fd5aa6eceb48ff047a7b8212a528c9f5fe0b
412099-OROZCO <412099@tecnicatura.frc.utn.edu.ar>
2025-05-23 20:23:44 -0300

    Add:  pom

--- src/main/java/com/bytechat/MongoHelper.java
+++ src/main/java/com/bytechat/MongoHelper.java
@@ -5,7 +5,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoHelper {
-    private static final String CONNECTION_STRING = "mongodb+srv://carlosenmanuelorozcon:<26090329>@bytechat.tkpe3ym.mongodb.net/?retryWrites=true&w=majority&appName=bytechat";
+    private static final String CONNECTION_STRING = "mongodb+srv://carlosenmanuelorozcon:<InserteContrasena>@bytechat.tkpe3ym.mongodb.net/?retryWrites=true&w=majority&appName=bytechat";
 
     public static MongoDatabase getDatabase() {
         MongoClient client = MongoClients.create(CONNECTION_STRING);
