6e791247d0c14269eeef5e488b6d2391b444081b
412099-OROZCO <412099@tecnicatura.frc.utn.edu.ar>
2025-05-24 15:26:40 -0300

    Fix:Emergency fix for deployment on render

--- src/main/java/com/bytechat/Main.java
+++ src/main/java/com/bytechat/Main.java
@@ -10,7 +10,9 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
 
         // Ruta para API
         server.createContext("/api/mensajes", new MensajeHandler());
